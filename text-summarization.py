156f0efb14349b7a873b67d3fa017dabb92374ce
pakagrong-lebel <92211586+pakagrong-lebel@users.noreply.github.com>
2023-10-29 16:09:55 +1100

    Add files via upload

---
+++ text-summarization.py
@@ -0,0 +1,3 @@
+# Python script for text summarization using NLP techniques
+# Your code here to read the text data and preprocess it (e.g., removing stop words)
+# Your code here to generate the summary using techniques like TF-IDF, TextRank, or BERT
